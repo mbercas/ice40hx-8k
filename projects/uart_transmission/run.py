1e7a817859cba32b755c3f7c2b1bc0e0ddf1d12c
mbercas <manuel@zeppo.localdomain>
2021-01-04 21:29:24 +0100

    Reorganize test bench under component sources

--- projects/uart_transmission/run.py
+++ projects/uart_transmission/run.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
+"""
+VHDL UART TX block
+"""
 
 from vunit import VUnit
+from pathlib import Path
+
 
 # Create VUnit instance by parsing command line arguments
 vu = VUnit.from_argv()
 
-# Create library 'lib'
-lib = vu.add_library("lib")
+SRC_PATH = Path(__file__).parent / "src"
+TEST_PATH = SRC_PATH / "test"
+
+# Create component library and testbench libary
+vu.add_library("uart_lib").add_source_files(SRC_PATH / "*.vhd")
+vu.add_library("tb_uart_lib").add_source_files(TEST_PATH / "*.vhd")
 
 # Add all files ending in .vhd in current working directory to library
 lib.add_source_files("*.vhd")
